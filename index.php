ccc0e27d95791cb15592c15465e050e5b236df98
kadidiatoukonte <kontekadidiatou@yahoo.fr>
2018-09-11 09:42:00 +0200

    includeFooter.php

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <!doctype html>
-<html class="no-js" lang="">
+<html class="no-js" lang="fr">
 
 <head>
   <meta charset="utf-8">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
-  <title></title>
+  <title>Baskets For Street</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/css/bootstrap.min.css" integrity="sha384-Smlep5jCw/wG7hdkwQ/Z5nLIefveQRIY9nfy6xoR1uRYBtpZgI6339F5dgvm/e9B" crossorigin="anonymous">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.1/css/all.css" integrity="sha384-O8whS3fhG2OnA5Kas0Y9l3cfpmYjapjI0E4theH4iuMD+pLhbf6JI0jIMfYcK3yZ" crossorigin="anonymous">
   <meta name="description" content="">
@@ -21,12 +21,68 @@
 </head>
 
 <body>
-  <!--[if lte IE 9]>
-    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
-  <![endif]-->
 
-  <!-- Add your site or application content here -->
-  <p>Hello world! This is HTML5 Boilerplate.</p>
+  <div class="card-deck">
+  <div class="card">
+    <a href="#">
+    <div class="card-body">
+      <h5 class="card-title">Vans Old Skool W</h5>
+    </div>
+    <img class="card-img-top basket" src="img/basket_femme.jpg" alt="Card image cap">
+  <p class="none">Description</p>
+    <div class="card-footer">
+      <strong class="text-muted">À partir de 80,00€</strong>
+      <p class="none">Disponibilité</p>
+    </div>
+  </a>
+  </div>
+  <div class="card">
+    <a href="#">
+    <div class="card-body">
+      <h5 class="card-title">Victoria Deportivo Pu Contraste</h5>
+    </div>
+      <img class="card-img-top basket" src="img/basket_homme.jpg" alt="Card image cap">
+    <p class="none">Description</p>
+    <div class="card-footer">
+      <strong class="text-muted">À partir de 68,00€</strong>
+      <p class="none">Disponibilité</p>
+    </div>
+</a>
+</div>
+
+  <div class="card">
+    <a href="#">
+    <div class="card-body">
+      <h5 class="card-title">Veja Esplar Small Velcro</h5>
+    </div>
+      <img class="card-img-top basket" src="img/basket_fille.jpg" alt="Card image cap">
+      <p class="none">Description</p>
+    <div class="card-footer">
+      <strong class="text-muted">À partir de 75,00€</strong>
+      <p class="none">Disponibilité</p>
+    </div>
+</a>
+</div>
+  <div class="card">
+    <a href="#">
+    <div class="card-body">
+      <h5 class="card-title">Geox J Kommodor B.C J825Pc</h5>
+    </div>
+    <img class="card-img-top basket" src="img/basket_garçon.jpg" alt="Card image cap">
+    <p class="none">Description</p>
+    <div class="card-footer">
+      <strong class="text-muted">À partir de 75,00€</strong>
+      <p class="none">Disponibilité</p>
+    </div>
+</a>
+</div>
+</div>
+
+<?php include("footer.php"); ?>
+
+
+
+
   <script src="js/vendor/modernizr-3.6.0.min.js"></script>
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
